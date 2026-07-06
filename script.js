aa1b72665fed43f372a357aa845a469a03b5fb68
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-07-06 11:10:38 +0200

    add forecast display and update search functionality

--- script.js
+++ script.js
@@ -12,9 +12,37 @@ async function getData() {
 
         const data = await response.json();
         console.log(data)
+
+        const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=68c1ed9eb4f2cd7999faef05b5d5b90f`)
+        const forecast = await secondResponse.json();
+        showForecast(forecast)
     }
 
-    catch(error) {
+    catch (error) {
         console.log(error)
     }
 }
+
+function showForecast(forecast) {
+    const forecastDisplay = document.getElementById("forecast-container");
+    const forecastTime = forecast.list.filter(item => item.dt_txt.includes("15:00:00"));
+    console.log(forecastTime)
+    forecastTime.map(item => {
+        const day = new Date(item.dt_txt).toLocaleDateString("nl-NL", { weekday: "short" })
+        console.log(day)
+
+        forecastDisplay.innerHTML += `
+            <ul class="forecast-list">
+                 <li>
+                    <span class="label">${day}</span>
+                    <span class="icon"></span>
+                    <span class="value">${forecast.temp}</span>
+                </li>
+            </ul>
+    `
+    })
+
+
+
+}
+
