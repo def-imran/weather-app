57d7c731198ae922607611201582bca9663fd9c7
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-07-08 11:17:43 +0200

    adding sunrise and sunset function and background image to body

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ async function getData() {
         const data = await response.json();
         console.log(data)
         showWeatherToday(data);
+        showSunData(data);
 
         const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=68c1ed9eb4f2cd7999faef05b5d5b90f`)
         const forecast = await secondResponse.json();
@@ -24,7 +25,7 @@ async function getData() {
     }
 }
 
-function showWeatherToday(data){
+function showWeatherToday(data) {
     const humidityEl = document.getElementById("humidity");
     const humidityData = data.main.humidity
     humidityEl.innerText = humidityData;
@@ -40,13 +41,30 @@ function showWeatherToday(data){
 
 }
 
+function showSunData(data) {
+    const sunriseEl = document.getElementById("sunrise");
+    const sunriseTimestamp = data.sys.sunrise;
+    const dateSunrise = new Date(sunriseTimestamp * 1000);
+    const sunriseTime = dateSunrise.toLocaleTimeString('nl-NL', { hour: '2-digit', minute: '2-digit' });
+
+    sunriseEl.innerText = sunriseTime;
+
+    const sunsetEl = document.getElementById("sunset");
+    const sunsetTimestamp = data.sys.sunset;
+    const dateSunset = new Date(sunsetTimestamp * 1000);
+    const sunsetTime = dateSunset.toLocaleTimeString('nl-NL', { hour: '2-digit', minute: '2-digit' });
+
+    sunsetEl.innerText = sunsetTime;
+
+}
+
 
 function showForecast(forecast) {
     const forecastDisplay = document.getElementById("forecast-container");
     const forecastDataFiltert = forecast.list.filter(item => item.dt_txt.includes("15:00:00"));
     console.log(forecastDataFiltert);
     forecastDisplay.innerHTML = ``;
-    
+
     forecastDataFiltert.map(item => {
         const day = new Date(item.dt_txt).toLocaleDateString("en-EN", { weekday: "short" })
         console.log(day)
